6570da833417aa7fbcd80581dc256ed92db0af64
Lee Yul <gpal0110naver.com>
2022-09-14 21:09:58 +0900

    first push

---
+++ src/main/java/com/controller/HomeController.java
@@ -0,0 +1,28 @@
+package com.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+    public String home() {
+		System.out.println("home controller start");
+                   
+        return "/index";
+    }
+	
+	@RequestMapping(value = "/yul", method = RequestMethod.GET)
+    public String menu() {
+                   
+        return "/yul";
+    }
+	
+	@RequestMapping(value = "/yul/name")
+    public String log() {
+                   
+        return "/yul";
+    }
+}
