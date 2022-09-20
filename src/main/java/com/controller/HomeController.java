59ddb2ca4434f759b35e823b9147884ee14e3ec9
snj <kswoah123@naver.com>
2022-09-20 23:46:38 +0900

    마이바티스 데이터베이스 샘플

--- src/main/java/com/controller/HomeController.java
+++ src/main/java/com/controller/HomeController.java
@@ -1,18 +1,52 @@
 package com.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.service.TestService;
+
 @Controller
 public class HomeController {
 
+	@Autowired
+	TestService testService;
+	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-    public String main() {
+    public String main(String no) {
+		
+		String ab = testService.selectTest(no);
+		
+		System.out.println(ab+"이쁜이");
+		
 		
         return "home/main";
     }
 
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	//사용하지않는 컨트롤러 사후 삭제처리 ////////////////////////////////////////////////////////////////////////////
 	@RequestMapping(value = "/about", method = RequestMethod.GET)
---
+++ src/main/java/com/service/TestService.java
@@ -0,0 +1,8 @@
+package com.service;
+
+public interface TestService {
+
+
+	String selectTest(String no);
+
+}
