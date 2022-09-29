4978773f23c287f3e4f021249de017a0afe09b2f
snj <kswoah123@naver.com>
2022-09-29 12:41:45 +0900

    공통 자바스크립트 추가

--- src/main/resources/static/js/common/common.js
+++ src/main/resources/static/js/common/common.js
@@ -1,9 +1,44 @@
 (function() {
 	
- 	window.yul = function() {
-		//초기화
-
-
+ 	yul.common = function() {
+		//공통메소드 생성
 	};
+	
+	//공통 검증
+	yul.common.valid = function(type, checkValue) {
+		if(type != undefined && type != " "){
+		 	alert("검증 시도");
+		}else{
+			console.log("벨리데이션 적용에 실패 하였습니다.");
+		}
+	}
+	
+	//zipcode 간편 생성
+	yul.common.zipcode = function(setTag) {
+		if(setTag != undefined && setTag != " "){
+		 	alert("집코드 라이브러리");
+		}else{
+			console.log("zipcode 적용에 실패 하였습니다.");
+		}
+	}
+	
+	//ajax 간편 구현
+	yul.common.baseAjax = function(sendData, method) {
+		if(sendData != undefined && sendData != " "){
+		 	alert("에이작스 사용");
+			
+		}else{
+			console.log("데이터가 존재하지 않거나 잘못된 데이터 입니다.");			
+		}
+	}
+ 	
+ 	//popup창 간편 구현
+	yul.common.basePopup = function(setTag, msg) {
+	 	if(setTag != undefined && setTag != " "){
+		 	alert("팝업창 세팅");
+		}else{
+			console.log("팝업창 생성을 실패하였습니다.");			
+		}
+	}
  	
 })();
