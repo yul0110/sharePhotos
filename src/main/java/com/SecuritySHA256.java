ecc7536aa3efebde73eb93043da861910c1a1d1d
snj <kswoah123@naver.com>
2022-10-17 19:49:35 +0900

    비밀번호 암호화 복호화

---
+++ src/main/java/com/SecuritySHA256.java
@@ -0,0 +1,23 @@
+package com;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class SecuritySHA256 {
+
+	public String encrypt(String text) throws NoSuchAlgorithmException {
+        MessageDigest md = MessageDigest.getInstance("SHA-256");
+        md.update(text.getBytes());
+
+        return bytesToHex(md.digest());
+    }
+
+    private String bytesToHex(byte[] bytes) {
+        StringBuilder builder = new StringBuilder();
+        for (byte b : bytes) {
+            builder.append(String.format("%02x", b));
+        }
+        return builder.toString();
+    }
+	
+}
