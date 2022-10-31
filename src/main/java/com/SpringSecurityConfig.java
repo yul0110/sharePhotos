d24f2185a81cbae5f1618111e76dd74945b3e7fe
snj <kswoah123@naver.com>
2022-10-31 18:14:49 +0900

    설정추가

--- src/main/java/com/SpringSecurityConfig.java
+++ src/main/java/com/SpringSecurityConfig.java
@@ -9,22 +9,34 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 @EnableWebSecurity
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
-                    .antMatchers("/**").permitAll()    // LoadBalancer Chk
-                    .anyRequest().authenticated()
-                .and()
-                    .formLogin()
-                    .defaultSuccessUrl("/view/dashboard", true)
-                    .permitAll()
-                .and()
-                    .logout();
-    }
-
-    @Override
+	
+	@Override
     public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/static/js/**","/static/css/**","/static/img/**","/static/frontend/**");
+        web.ignoring().antMatchers("/static/js/**","/static/css/**","/static/images/**","/static/favicons/**","/static/font/**");
     }
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+	    http
+	            .authorizeRequests()
+	                .antMatchers("/**").permitAll()    // LoadBalancer Chk
+	                .anyRequest().authenticated()
+	            .and()
+	                .formLogin()
+	                .loginPage("/login")                                        // 사용자 정의 로그인 페이지
+	                //.defaultSuccessUrl("")                                // 로그인 성공 후 이동 페이지
+	                //.failureUrl("")                                       // 로그인 실패 후 이동 페이지
+	                .usernameParameter("userId")                                // 아이디 파라미터명 설정
+	                .passwordParameter("pw")                                // 패스워드 파라미터명 설정
+	                .loginProcessingUrl("/")                               // 로그인 Form Action Url
+	                .successHandler((request, response, authentication) -> {}) // 로그인 성공 후 핸들러
+	                .failureHandler((request, response, exception) -> {})     // 로그인 실패 후 핸들러
+	            .and()
+	                .logout()
+	                .logoutUrl("/")                                                    // 로그아웃 처리 URL
+	                .logoutSuccessUrl("/")                                             // 로그아웃 성공 후 이동페이지
+	                .deleteCookies("/")                                                // 로그아웃 후 쿠키 삭제
+	                .addLogoutHandler((request, response, authentication) -> {})      // 로그아웃 핸들러
+	                .logoutSuccessHandler((request, response, authentication) -> {}); // 로그아웃 성공 후 핸들러;
+	}
 }
